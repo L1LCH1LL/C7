8b0fb88efcbc03234dcdd5c85f79f7d95deeb6c6
L1LCH1LL <108305081+L1LCH1LL@users.noreply.github.com>
2023-02-07 03:11:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,33 +1,32 @@
-//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
 
 Console.Write("Введите размер m:  ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите размер n:  ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int[,] arr = new int[m, n];
+double[,] arr = new double [m, n];
 for (int i = 0; i < arr.GetLength(0); i++)
 {
     for (int j = 0; j < arr.GetLength(1); j++)
     {
-        arr[i, j] = new Random().Next(-9, 10);
+        var a = new Random().NextDouble();
+        arr[i, j] = new Random().Next(-9,10) / a + 0.3;
     }
 }
 
+PrintArr(arr);
 
 
-    Console.Write("Введите размер m1:  ");
-    int m1 = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите размер n1:  ");
-    int n1 = Convert.ToInt32(Console.ReadLine());
-    for (int i = 1; i < arr.GetLength(0); i++)
+void PrintArr(double[,] arry)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 1; j < arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write($"{arr[i, j], 10}");
+            Console.Write($"{ Math.Round(arry[i, j], 2), 15}");
         }
         Console.WriteLine();
     }
-    if ( m1<m && n1<n ) Console.Write($"{arr[m1, n1], 10}");
-    else Console.Write("Нэт такого числа !");
-
+}
